0944f10d1df751d6a06a4a6dae710af23a40097c
ilhanTabak <herr.ilhantabak@gmail.com>
2023-02-24 21:00:08 +0100

    Tetst Uploaded

---
+++ src/Test/OrtakDosya.java
@@ -0,0 +1,6 @@
+package Test;
+
+public class OrtakDosya {
+    //bunu mainBranchta iken olusturdum ama add ve commit yapmadim.
+    //test branchi lokalden push ediliyor
+}
---
+++ src/Test/yeniTest.java
@@ -0,0 +1,5 @@
+package Test;
+
+public class yeniTest {
+    //lokal test yazildi
+}

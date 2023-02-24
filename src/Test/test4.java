fa9d19dc88a3ebb7cac469cf4f2cc81ac28c817f
ilhan Tabak <121499527+ilhanTabak@users.noreply.github.com>
2023-02-24 21:13:15 +0100

    Update test4.java

--- src/Test/test4.java
+++ src/Test/test4.java
@@ -7,5 +7,6 @@ public class test4 {
         System.out.println("test 2");
         System.out.println("test degis");
         System.out.println("dhdjd");
+        //her havuzun dibi ayni
     }
 }
